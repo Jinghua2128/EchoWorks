63c3fd6acc36b07f158d920b5b124c672aa4352b
Liu GuangXuan <s10268212@connect.np.edu.sg>
2026-07-01 13:30:24 +0800

    V0.1.6 Firebase

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBWmLMcTnFbvOJ8p5klUdFgunatA68j46k",
+  authDomain: "echoworks-e3b4d.firebaseapp.com",
+  databaseURL: "https://echoworks-e3b4d-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "echoworks-e3b4d",
+  storageBucket: "echoworks-e3b4d.firebasestorage.app",
+  messagingSenderId: "1036430593532",
+  appId: "1:1036430593532:web:6e27f85ca4a115c1a97679",
+  measurementId: "G-VFB8L0Y2NC"
+};
